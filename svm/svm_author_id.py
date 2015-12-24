ad419096808279c5c0664e997190f60de27f7c02
Ted <tedsamore@gmail.com>
2015-12-24 11:44:08 -0600

    Everything without the dataset

---
+++ svm/svm_author_id.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 2 (SVM) mini-project.
+
+    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+
+
+#########################################################
+### your code goes here ###
+
+#########################################################
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import pdb
+
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
+# c_arr = [10, 100, 1000, 10000]
+c_arr = [10000]
+arr_prediction = [9, 25, 49]
+for c in c_arr:
+	print 'C value ' + str(c)
+	clf = svm.SVC(kernel = 'rbf', C = c)
+
+	t0 = time()
+	clf.fit(features_train, labels_train)
+	print "training time:", round(time()-t0, 3), "s"
+
+	t0 = time()
+	pred = clf.predict(features_test)
+	print "prediction time:", round(time()-t0, 3), "s"
+
+
+	print accuracy_score(pred, labels_test)
+	for pr in arr_prediction: 
+		print 'Element ' + str(pr) + str(pred[pr])
+	pdb.set_trace()
+	plt.plot([1,2,3,4], [1,4,9,16], 'ro')
+	plt.axis([0, 6, 0, 20])
+	plt.show()
